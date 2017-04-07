1ca5d504ef8034428e0d6eb78c0640d62a2111d0
Caoilte O'Connor <caoilte@gmail.com>
2017-04-07 11:29:10 +0100

    Fix Environments Parser so that it rejects invalid Environments - Previously it only used valid Environments as Examples, now it uses them for validation (yay!)

--- sbt-chuckwagon/src/main/scala/com/itv/chuckwagon/sbt/Parsers.scala
+++ sbt-chuckwagon/src/main/scala/com/itv/chuckwagon/sbt/Parsers.scala
@@ -8,15 +8,16 @@ import sbt.complete.DefaultParsers._
 import sbt.complete.FixedSetExamples
 import sbt.complete.Parser
 
-import scala.collection.immutable.Seq
-
 object Parsers {
 
   val environmentArgParser: Def.Initialize[Parser[AliasName]] = Def.setting {
-    val environments: Seq[String] = chuckEnvironments.value.map(_.name).toList
-    val environmentParser: Parser[AliasName] =
-      token(NotQuoted.examples(FixedSetExamples(environments))).map(AliasName)
-    token(' ') ~> environmentParser
+    val environments: List[String] = chuckEnvironments.value.map(_.name).toList
+
+    val environmentStringParser: Parser[String] =
+      environments.foldLeft[Parser[String]](failure("Invalid input."))((e1, e2) => e1 | e2)
+    val environmentExamplesParser: Parser[AliasName] =
+      token(environmentStringParser.examples(FixedSetExamples(environments))).map(AliasName)
+    token(' ') ~> environmentExamplesParser
   }
 
   val versionArgParser: Def.Initialize[Parser[LambdaVersion]] = Def.setting {
